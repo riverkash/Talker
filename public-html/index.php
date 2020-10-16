169b5bf1cd1d7a0ba02fc3d610a4989cb118c1e1
Kev Harris <riverkash@me.com>
2020-10-16 14:35:39 +0100

    added sign up form to index.php

--- public-html/index.php
+++ public-html/index.php
@@ -5,13 +5,45 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+    <title>TALKER</title>
+
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-
-    <title>Hello, world!</title>
   </head>
+
+
   <body>
-    <h1>Hello, world!</h1>
+    <div class="container">
+      <div class="row justify-content-md-center">
+        <div class="col-12 col-md-auto">
+          <h1>Talker | Sign up</h1>
+        </div>
+      </div>
+      <hr><br>
+      <div class="row">
+        <div class="col-6">
+          <form action="">
+            <div class="form-group">
+              <label for="formSignUpEmail">Email Address</label>
+              <input type="email" class="form-control" id="formSignUpEmail" placeholder="Enter your Email Address">
+            </div>
+            <div class="form-group">
+              <label for="formSignUpPassword">Password</label>
+              <input type="password" class="form-control" id="formSignUpPassword" placeholder="Enter your Password">
+
+              <input type="password" class="form-control mt-4" id="formSignUpPasswordConf" placeholder="Confirm your Password">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </form>
+        </div>
+        <div class="col-6">
+          <p>Hello and welcome to talker! We are very happy that you want to join our great community!</p>
+          <p>Please, enter your email and password. You must have access to your email as we will send a confirmation code to that address. Your password must be between 8 and 16 characters in length, with at least one UPPERCASE and one lowercase character, one number and one special character (@, *, $ or #).</p>
+          <p>We hope you'll enjoy Talker.</p>
+        </div>
+      </div>
+    </div>
+    
 
     <!-- Optional JavaScript; choose one of the two! -->
 
